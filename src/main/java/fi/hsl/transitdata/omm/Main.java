e1291a5d057160867407c0bea6e6218914288aba
Anton Jyrkiäinen <anton.jyrkiainen@helsinki.fi>
2023-10-15 19:22:19 +0300

    updated env

--- src/main/java/fi/hsl/transitdata/omm/Main.java
+++ src/main/java/fi/hsl/transitdata/omm/Main.java
@@ -84,11 +84,7 @@ public class Main {
     private static String readConnectionString() throws Exception {
         String connectionString = "";
         try {
-            //Default path is what works with Docker out-of-the-box. Override with a local file if needed
-            final String secretFilePath = ConfigUtils.getEnv("FILEPATH_CONNECTION_STRING")
-                                                     .orElse("/run/secrets/db_conn_string");
-            connectionString = new Scanner(new File(secretFilePath))
-                    .useDelimiter("\\Z").next();
+            connectionString = System.getenv("TRANSITDATA_PUBTRANS_CONN_STRING");
         } catch (Exception e) {
             log.error("Failed to read DB connection string from secrets", e);
             throw e;
